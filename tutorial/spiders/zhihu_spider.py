8c78ad5e669cdc39137cfec1183bc893143af453
Lawrence Liu <suo.lawrence.liu@gmail.com>
2015-04-14 22:58:40 -0400

    added the function of parsing the url of next page, now the spider can download all answers of a specific zhihu user

--- tutorial/spiders/zhihu_spider.py
+++ tutorial/spiders/zhihu_spider.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 from tutorial.items import question_link, answer
@@ -17,9 +18,12 @@ class ZhihuSpider(Spider):
 	def parse(self, response):  
 		item = question_link()
 		sel = Selector(response)
-		print 'hehe'
+		next_link = sel.xpath(u'//span/a[text()="下一页"]/@href').extract()
+		if next_link:
+			next_link = self.start_urls[0] + next_link[0] 
+			yield Request(url = next_link, callback = self.parse)
+
 		for link in sel.xpath('//a[@class="question_link"]/@href').extract():
-			print link
 			yield Request(url = 'http://www.zhihu.com' + link, callback = self.parse_answer)
 
 	
@@ -29,7 +33,3 @@ class ZhihuSpider(Spider):
 		ans_item['question'] = response_selector.xpath('//title[1]/text()').extract()
 		ans_item['answer'] = response_selector.xpath('//div[@class = " zm-editable-content clearfix"][1]/text()').extract()
 		yield ans_item
-
-
-
-
