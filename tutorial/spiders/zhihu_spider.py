f74ab312306db6f1c0a7f121a8b518ca03c1f8dd
Lawrence Liu <suo.lawrence.liu@gmail.com>
2015-04-19 21:12:36 -0400

    added another spider: TopicSpider. This spider can scrape all top answers under a given topic. A small bug: If the answer is so short that it doesn't have a link of '显示全部' , the spider will ignore this answer.

--- tutorial/spiders/zhihu_spider.py
+++ tutorial/spiders/zhihu_spider.py
@@ -8,7 +8,7 @@ import urllib2
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-
+#todo: for now those text wrapped by any html tag is ignored. 
 class ZhihuSpider(Spider):
 	name = 'Zhihu'
 	#allowed_domains = 'zhihu.com'
@@ -30,6 +30,23 @@ class ZhihuSpider(Spider):
 	def parse_answer(self, response):
 		ans_item = answer()
 		response_selector = Selector(response)
-		ans_item['question'] = response_selector.xpath('//title[1]/text()').extract()
-		ans_item['answer'] = response_selector.xpath('//div[@class = " zm-editable-content clearfix"][1]/text()').extract()
+		ans_item['question'] = response_selector.xpath('//title[1]/node()').extract()
+		ans_item['answer'] = ''.join(response_selector.xpath('//div[@class = " zm-editable-content clearfix"][1]/node()').extract())
+		ans_item['upvote'] = response.xpath('//div[@class="zm-item-vote-info "]/@data-votecount')[0].extract()
 		yield ans_item
+
+
+class TopicSpider(ZhihuSpider):
+	name = 'Topic'
+	def __init__(self, topic = None):
+		self.start_urls = ['http://www.zhihu.com/topic/' + str(topic) + '/top-answers']
+
+	def parse(self, response):
+		sel = Selector(response)
+		next_link = sel.xpath(u'//span/a[text()="下一页"]/@href').extract()
+		if next_link:
+			next_link = self.start_urls[0] + next_link[0] 
+			yield Request(url = next_link, callback = self.parse)
+
+		for link in sel.xpath(u'//a[text()="显示全部"]/@href').extract():
+			yield Request(url = 'http://www.zhihu.com' + link, callback = self.parse_answer)
